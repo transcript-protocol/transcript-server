3690d1d9633f058176ac1bb8f9f6851e91cc9193
Will Galebach <willbach@gmail.com>
2018-07-16 10:36:31 +0000

    Added bcrypt password validation (#3)

--- src/entities/User.js
+++ src/entities/User.js
@@ -1,3 +1,6 @@
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcrypt-nodejs');
+
 const mongoose = require('mongoose')
 mongoose.Promise = require('bluebird')
 mongoose.createConnection('mongodb://localhost/gmblr')
@@ -13,16 +16,53 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
     username: { //suggest school email for GC and not school email for student
         type: String,
-        unique: true
+        required: true,
+        index: { unique: true }
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     accountType: {
-        type: String
+        type: String,
+        required: true
     },
     sequence: Number,
     updatedAt: { type: Date, default: Date.now }
 })
+
+UserSchema.pre('save', function(next) {
+    const user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) {
+            return next(err);
+        }
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) {
+                return next(err);
+            }
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            return next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', UserSchema)
