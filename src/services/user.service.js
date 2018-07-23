659bf72e42693482a6c434a5135a66845c5ee306
acuddeback <atc@cuddeback.org>
2018-07-23 13:43:20 -0400

    just tryna fix these conflicts!!

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -136,8 +136,10 @@ const guidance1 ={
     hashValue: 'cbe3d16cc9f5cef09648e350a1abfbd4a3fb02b7a7f1cd6c02c23b5ee9857e58',
     username: 'euler@python.com'
     studentUsername: 'student@emblemEDU.com'
+=======
 }
 */
+
 
 //get hash
 userService.getHash = (hashValue) => {
--- test/server.spec.js
+++ test/server.spec.js
@@ -170,7 +170,6 @@ describe('HTTP Server', function() {
     //     }) 
     // })
 
-    
 })
 
 
